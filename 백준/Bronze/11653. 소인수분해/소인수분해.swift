2c05e6ceadc571fd44bef332b122f667cdb1667c
Hun <84387602+Hun-322@users.noreply.github.com>
2023-02-05 16:32:41 +0900

    [Bronze I] Title: 소인수분해, Time: 12 ms, Memory: 69100 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.swift
+++ 백준/Bronze/11653. 소인수분해/소인수분해.swift
@@ -1,22 +1,15 @@
-import Foundation
-
 var N = Int(readLine()!)!
-var arr = Array(repeating: true, count: N + 1)
-var isPrime:[Int] = []
-
-if N == 1 {
-    exit(0)
-}
-
-for i in 2...N {
-    if arr[i] == true {
-        for j in stride(from: i, through: N, by: i) {
-            arr[j] = false
-        }
-        
-        while N % i == 0 {
-            N /= i
-            print(i)
-        }
+var num = 2
+    
+while N > 1 {
+    if num * num > N {
+        print(N)
+        break
     }
+    
+    while N % num == 0 {
+        N /= num
+        print(num)
+    }
+    num += 1
 }
