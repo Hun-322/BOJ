022d61dedfe572c6529c81dd50c4b1755d0216b3
Hun <ksh26271@naver.com>
2023-02-06 21:14:01 +0900

    [Silver II] Title: 베르트랑 공준, Time: 24 ms, Memory: 69344 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.swift
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.swift
@@ -1,22 +1,25 @@
+var arr = Array(repeating: true, count: 246913)
+
+for i in 2...246912 {
+    if arr[i] == true {
+        for j in stride(from: i*i, through: 246913, by: i) {
+            arr[j] = false
+        }
+    }
+}
+
 while true {
     var n = Int(readLine()!)!
     var sum = 0
-    var arr = Array(repeating: true, count: n*2 + 1)
-    
+
     if n == 0 {
         break
     }
     
-    for i in 2..<arr.count {
-        if arr[i] == true {
-            for j in stride(from: i, through: arr.count-1, by: i) {
-                arr[j] = false
-            }
-            if i > n {
-                sum += 1
-            }
+    for i in n+1...n*2 {
+        if arr[i] {
+            sum += 1
         }
-        
     }
     print(sum)
 }
