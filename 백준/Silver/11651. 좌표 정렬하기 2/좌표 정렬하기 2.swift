07d2dbbc8f9facd548ab903eda0bddcf207c7671
Hun <ksh26271@naver.com>
2023-02-13 00:26:07 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 260 ms, Memory: 77276 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.swift
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.swift
@@ -1,18 +1,16 @@
 var n = Int(readLine()!)!
-var tuples: [(Int,Int)] = []
+var arr = Array(repeating: Array(repeating: 0, count: 2), count: n)
 
-for _ in 0..<n {
-    var input = readLine()!.split(separator: " ").map{Int($0)!}
-    tuples.append((input[0], input[1]))
+for i in 0..<n {
+    arr[i] = readLine()!.split(separator: " ").map{Int($0)!}
 }
-
-tuples = tuples.sorted(by: {
-    if $0.1 == $1.1 {
-        return $0.0 < $1.0
+arr = arr.sorted(by: {
+    if $0[1] == $1[1] {
+        return $0[0] < $1[0]
     }else {
-        return $0.1 < $1.1
+        return $0[1] < $1[1]
     }})
 
-for i in tuples {
-    print(i.0, i.1)
+for i in arr {
+    print(i.map{String($0)}.joined(separator: " "))
 }
