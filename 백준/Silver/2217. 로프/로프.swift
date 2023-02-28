1c13a4ec7011f69f2625b28571048d3b7b3247a8
Hun <ksh26271@naver.com>
2023-03-01 03:33:47 +0900

    [Silver IV] Title: 로프, Time: 60 ms, Memory: 71324 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.swift
@@ -0,0 +1,17 @@
+let N = Int(readLine()!)!
+var arr = [Int]()
+
+for _ in 1...N {
+    arr.append(Int(readLine()!)!)
+}
+
+arr = arr.sorted(by: >)
+var max = arr[0]
+
+for i in 0..<arr.count {
+    
+    if max < arr[i] * (i+1) {
+        max = arr[i] * (i+1)
+    }
+}
+print(max)
