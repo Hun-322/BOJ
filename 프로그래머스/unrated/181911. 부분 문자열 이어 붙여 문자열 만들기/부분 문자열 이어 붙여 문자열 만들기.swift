66384def9fc4bcb5256649882939650a0deeb73b
Hun <ksh26271@naver.com>
2023-11-15 12:48:57 +0900

    [level unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 1.86 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+func solution(_ my_strings:[String], _ parts:[[Int]]) -> String {
+    var answer = [String]()
+    
+    for i in 0..<my_strings.count {
+        answer += my_strings[i].map { String($0) }[parts[i][0]...parts[i][1]]
+    }
+    
+    return answer.joined()
+}
