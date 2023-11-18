5e23aef1f31785247dc17c9d3e2f97c17078c1e7
Hun <ksh26271@naver.com>
2023-11-18 18:20:39 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.swift
@@ -4,9 +4,9 @@ func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
     var arr1 = cards1, arr2 = cards2
     
     for word in goal {
-        if arr1 != [], arr1[0] == word {
+        if !arr1.isEmpty, arr1[0] == word {
             arr1.removeFirst()
-        } else if  arr2 != [], arr2[0] == word {
+        } else if !arr2.isEmpty, arr2[0] == word {
             arr2.removeFirst()
         } else {
             return "No"
