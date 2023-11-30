95e576c86902c58755bbf10b5a81d7ce0ca71cac
Hun <ksh26271@naver.com>
2023-11-30 15:20:08 +0900

    [level 2] Title: 프로세스, Time: 1.88 ms, Memory: 16.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+func solution(_ priorities:[Int], _ location:Int) -> Int {
+    var arr = priorities
+    var idx = location
+    var answer = 0
+    
+    while !arr.isEmpty {
+        print(arr)
+        print(idx)
+        let maxValue = arr.max()!
+        idx -= 1
+        
+        if maxValue != arr[0] {
+            arr.append(arr.removeFirst())
+            
+            if idx < 0 {
+                idx = arr.count - 1
+            }
+        } else {
+            answer += 1
+            arr.removeFirst()
+            
+            if idx < 0 {
+                break
+            }
+        }
+    }
+    return answer
+}
